9ad34fff07b02ae34bec8d594c085a1ed58fca55
Abideen Olafimihan <olafimihana@gmail.com>
2024-02-06 17:52:17 +0100

    profile page initiated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
+import useCurrentUser from "@/hook/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
 
 export default function Home() {
+  const { data } = useCurrentUser();
+
   return (
     <>
       <h1 className="text-2xl text-green-500">NETFLIX CLONE</h1>
+
+      <p className="text-white">Logged in as: {data?.email}</p>
 
       <button
         className="border border-green-500 w-full bg-white"
