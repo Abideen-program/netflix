62439f9a46fec56a4850a188a79892364fdf6c52
Abideen Olafimihan <olafimihana@gmail.com>
2024-02-08 08:55:47 +0100

    navbar initiated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,12 @@
 import useCurrentUser from "@/hook/useCurrentUser";
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
 
 export default function Home() {
-  const { data } = useCurrentUser();
 
   return (
     <>
-      <h1 className="text-2xl text-green-500">NETFLIX CLONE</h1>
-
-      <p className="text-white">Logged in as: {data?.name}</p>
-
-      <button
-        className="border border-green-500 w-full bg-white"
-        onClick={() => signOut()}
-      >
-        Sign Out
-      </button>
+      
     </>
   );
 }
