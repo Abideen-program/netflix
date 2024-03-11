fb6fc6078a47151b9f15777e3878ca56f929738e
Abideen Olafimihan <olafimihana@gmail.com>
2024-03-11 22:55:23 +0100

    favorite button

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -3,16 +3,50 @@ import axios from "axios";
 
 import useCurrentUser from "@/hook/useCurrentUser";
 import useFavorites from "@/hook/useFavorites";
-import { AiOutlinePlus } from "react-icons/ai";
+import { AiOutlineCheck, AiOutlinePlus } from "react-icons/ai";
 
 interface FavoriteButtonProps {
   movieId: string;
 }
 
 export const FavoriteButton = ({ movieId }: FavoriteButtonProps) => {
+  const { mutate: mutateFavorites } = useFavorites();
+  const { data: currentUser, mutate } = useCurrentUser();
+
+  const isFavorite = useMemo(() => {
+    const list = currentUser?.favoriteIds || [];
+
+    return list.includes(movieId);
+  }, [currentUser, movieId]);
+  
+
+  const toggleFavorites = useCallback(async () => {
+    let response;
+
+    if (isFavorite) {
+      response =   axios.delete("/api/favorite", { data: { movieId } });
+    } else {
+      response =   axios.post("/api/favorite", { movieId });
+    }
+    console.log(response);
+    const updatedFavoriteIds = response?.data?.favoriteIds;
+
+    mutate({
+      ...currentUser,
+      favoriteIds: updatedFavoriteIds,
+    });
+
+    mutateFavorites();
+  }, [movieId, currentUser, isFavorite, mutate, mutateFavorites]);
+
+  const Icon = isFavorite ? AiOutlineCheck : AiOutlinePlus;
+
   return (
-    <div className="w-6 h-6 lg:w-10 lg:h-10 border-2 border-white rounded-full cursor-pointer hover:border-neutral-300 transition flex items-center justify-center">
-      <AiOutlinePlus className="text-white" size={25} />
+    <div
+      onClick={toggleFavorites}
+      className="w-6 h-6 lg:w-10 lg:h-10 border-2 border-white rounded-full cursor-pointer hover:border-neutral-300 transition flex items-center justify-center"
+    >
+      <Icon className="text-white" size={25} />
     </div>
   );
 };
